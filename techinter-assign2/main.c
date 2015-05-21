3cd354f10d633c40e33dff364f7e7e145441bdd2
srohani <srohani@gmail.com>
2015-05-20 22:54:15 -0700

    Initial Commit

---
+++ techinter-assign2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  techinter-assign2
+//
+//  Created by Samer Rohani on 2015-05-20.
+//  Copyright (c) 2015 Samer Rohani. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
